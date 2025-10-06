4115e85f7b09022482777fd4b4813beb6f0fa35c
MacBook Pro <macbookpro@MacBookPro.fritz.box>
2025-10-06 14:01:21 +0200

    clean up url while hosting

--- utils/js/imgopt.js
+++ utils/js/imgopt.js
@@ -1,9 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
+
+
+
+
+(function ($) {
+
+
+  document.addEventListener("DOMContentLoaded", () => {
   const loader = document.getElementById("page-loader");
   const imgs = document.querySelectorAll("img.blur-up");
   let loadedCount = 0;
 
-  const regex = /e_blur:200,q_auto,f_auto,w_330,h_440/;
 
   if (imgs.length === 0) {
     // no images, hide loader immediately
@@ -26,7 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
     fullImg.onload = () => {
       img.src = fullImg.src;
       img.classList.add("loaded");
-      img.src = img.src.replace(regex, "e_blur:0,q_auto,f_auto");
+      img.src = cleanUrl(img.src);
 
       loadedCount++;
       if (loadedCount === imgs.length && loader) {
@@ -42,3 +48,10 @@ document.addEventListener("DOMContentLoaded", () => {
     };
   });
 });
+
+
+function cleanUrl(url) {
+  const regex = `/e_blur:200%2Cq_auto%2Cf_auto%2Cw_330%2Ch_440`; // hosting
+  // const regex2 = `/e_blur:200,q_auto,f_auto,w_330,h_440`; // local testing
+  return url.replace(regex, '');
+}})(jQuery);
