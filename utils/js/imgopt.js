1991c7aefc5ecdfa1742cab2a6ea453827a1551c
MacBook Pro <macbookpro@MacBookPro.fritz.box>
2025-10-06 11:39:21 +0200

    image opt while loading and remove the config after it is loaded

--- utils/js/imgopt.js
+++ utils/js/imgopt.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const imgs = document.querySelectorAll("img.blur-up");
   let loadedCount = 0;
 
+  const regex = /e_blur:200,q_auto,f_auto,w_330,h_440/;
+
   if (imgs.length === 0) {
     // no images, hide loader immediately
     if (loader) {
@@ -24,10 +26,10 @@ document.addEventListener("DOMContentLoaded", () => {
     fullImg.onload = () => {
       img.src = fullImg.src;
       img.classList.add("loaded");
+      img.src = img.src.replace(regex, "e_blur:0,q_auto,f_auto");
 
       loadedCount++;
       if (loadedCount === imgs.length && loader) {
-        // loader.style.display = "none"; // hide loader when all done
         loader.classList.add("d-none");
       }
     };
